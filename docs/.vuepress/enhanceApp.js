322bc32650984eab0ad26e2431d9df49d67791ab
Natsu Houraisan <hello@neneko.cn>
2023-09-08 00:15:09 +0800

    fix navbar v2

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,29 +1,28 @@
 import Vue from 'vue';
 
 export default ({ router }) => {
-  router.beforeEach((to, from, next) => {
-    // 使用媒体查询来检测屏幕宽度
-    const isSmallScreen = window.matchMedia("(max-width: 992px)").matches;
+  // 在 Vue 的 `beforeCreate` 钩子中执行代码
+  Vue.mixin({
+    beforeCreate() {
+      // 检查是否在客户端环境中
+      if (typeof window !== 'undefined') {
+        const isSmallScreen = window.matchMedia("(max-width: 992px)").matches;
 
-    if (isSmallScreen) {
-      // 屏幕宽度小于等于 768px 时应用折叠效果
-      Vue.nextTick(() => {
-        const navbarCollapse = document.querySelector(".navbar-collapse");
-        const navbarToggler = document.querySelector(".navbar-toggler");
+        if (isSmallScreen) {
+          this.$nextTick(() => {
+            const navbarCollapse = document.querySelector(".navbar-collapse");
+            const navbarToggler = document.querySelector(".navbar-toggler");
 
-        if (navbarCollapse && navbarToggler) {
-          const isNavbarExpanded = navbarToggler.classList.contains("collapsed");
+            if (navbarCollapse && navbarToggler) {
+              const isNavbarExpanded = navbarToggler.classList.contains("collapsed");
 
-          if (!isNavbarExpanded) {
-            // 如果导航栏是展开状态，模拟点击折叠按钮
-            navbarToggler.click();
-          }
+              if (!isNavbarExpanded) {
+                navbarToggler.click();
+              }
+            }
+          });
         }
-        next();
-      });
-    } else {
-      // 屏幕宽度大于 768px 时不应用折叠效果
-      next();
+      }
     }
   });
 };
