0d2cafefba4a70d0467233670ab710630218ab7b
Natsu Houraisan <hello@neneko.cn>
2023-09-08 00:58:56 +0800

    fix navbar v3

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,28 +1,45 @@
 import Vue from 'vue';
 
 export default ({ router }) => {
-  // 在 Vue 的 `beforeCreate` 钩子中执行代码
-  Vue.mixin({
-    beforeCreate() {
-      // 检查是否在客户端环境中
-      if (typeof window !== 'undefined') {
-        const isSmallScreen = window.matchMedia("(max-width: 992px)").matches;
+  // 检查是否在客户端环境中
+  // if (typeof window !== 'undefined') {
+  //   const isSmallScreen = window.matchMedia("(max-width: 992px)").matches;
+  //   const navbarToggler = document.querySelector(".navbar-toggler");
+  //   const navbarCollapse = document.querySelector(".navbar-collapse");
 
-        if (isSmallScreen) {
-          this.$nextTick(() => {
-            const navbarCollapse = document.querySelector(".navbar-collapse");
-            const navbarToggler = document.querySelector(".navbar-toggler");
+  //   if (isSmallScreen && navbarToggler && navbarCollapse) {
+  //     // 在小屏幕上，点击按钮以切换导航栏的展开状态
+  //     navbarToggler.addEventListener("click", () => {
+  //       if (navbarCollapse.classList.contains("show")) {
+  //         navbarCollapse.classList.add("collapsing"); // 添加 collapsing 类
 
-            if (navbarCollapse && navbarToggler) {
-              const isNavbarExpanded = navbarToggler.classList.contains("collapsed");
+  //         setTimeout(() => {
+  //           navbarCollapse.classList.remove("show");
+  //           navbarCollapse.classList.remove("collapsing"); // 移除 collapsing 类
+  //           navbarCollapse.classList.add("collapse")
+  //         }, 350); // 此处的时间与 CSS 过渡的时间一致
+  //       } else {
+  //         navbarCollapse.classList.add("show");
 
-              if (!isNavbarExpanded) {
-                navbarToggler.click();
-              }
-            }
-          });
+  //       }
+  //     });
+  //   }
+  // }
+
+  router.beforeEach((to, from, next) => {
+    // 检查是否在客户端环境中
+    if (typeof window !== 'undefined') {
+      // 在路由切换后，将导航栏折叠（如果是小屏幕）
+      const isSmallScreen = window.matchMedia("(max-width: 992px)").matches;
+      const navbarCollapse = document.querySelector(".navbar-collapse");
+
+      if (isSmallScreen && navbarCollapse) {
+        if (navbarCollapse.classList.contains("show")) {
+          navbarCollapse.classList.remove("show");
         }
       }
     }
+
+    next();
   });
 };
